01a50df80862cd49f86bf95ccfffa55488e767c3
Yoonsuck Choe <choe@tamu.edu>
2022-10-28 13:46:11 -0500

    capitalization hack

--- abstractbook/mk-abstract-book.py
+++ abstractbook/mk-abstract-book.py
@@ -7,10 +7,20 @@ import pandas as pd
 import copy
 import re
 import math
+import sys
 
+if (len(sys.argv) != 2):
+  print("usage: ./mk-abstract-book.py <csv-filename>")
+  exit()
+
+#--------------------
 # CONFIG
+#--------------------
 year = 2022
-filename = '2018.csv'
+filename = sys.argv[1]	
+lastname = "Last name (in English)"    # column title for lastname
+firstname = "First name (in English)"  # column title for firsname
+#--------------------
 
 
 '''
@@ -22,7 +32,7 @@ def print_header (year):
 
   print("<h3>AKN Abstracts "+str(year)+"</h3>") 
   print("<table>")
-  print("<tr><b><td></td><td><b>Name</b></td> <td><b>Affiliation</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td> <b>Theme </b></td></b></tr>")
+  print("<tr><b><td></td><td><b>Name</b></td><td>&nbsp</td> <td><b>Affiliation</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td><b>Abst</b></td> <td> <b>Theme </b></td></b></tr>")
 
 
 def print_footer ():
@@ -40,13 +50,37 @@ def td_print_url (tag,url):
   print('<td> <a href="'+url+'">'+tag+'</a> </td>')
 
 
+def capitalize (name):
+  '''
+  quick hack to properly capitalize names
+  '''
+
+  words = re.split("\s",name)
+  # 1. two words
+  ret = ""
+  if (len(words)==2):
+    for w in words:
+       ret = ret+w.capitalize()+" "
+    ret = ret[:-1]
+  
+  words = re.split("-",ret)
+  # 2. A-B 
+  if (len(words)>1):
+    ret = ""
+    for w in words:
+       ret = ret+w.capitalize()+"-"
+    return ret[:-1]
+
+  # 3. Other  
+  return name.capitalize()
+
 '''
   
   1. load CSV file 
 
 '''
 df = pd.read_csv(filename)
-df = df.sort_values(by=['Lastname','Firstname'],ascending=True)
+df = df.sort_values(by=['Last name (in English)','First name (in English)'],ascending=True)
 
 '''
 
@@ -74,12 +108,12 @@ for idx in df.index:
 
   print('<tr>')
 
-  td_print(n)
+  td_print(str(n)+'&nbsp;')
   n+=1
 
   # 1. Name
-  td_print(df['Lastname'][idx].capitalize())
-  td_print(df['Firstname'][idx].capitalize())
+  td_print( capitalize(df['Last name (in English)'][idx]) )
+  td_print( capitalize(df['First name (in English)'][idx]) )
 
   # 2. Affiliation
   td_print(df['Affiliation'][idx])
@@ -91,7 +125,7 @@ for idx in df.index:
       td_print(' &nbsp; ')
     else:
       if (re.search("abstractsonline",url) and re.search("presentation", url)):
-        td_print_url('Abst'+str(i),url)
+        td_print_url('Abst'+str(i+1),url)
       else:
         td_print_url('invalid url',url)
 
